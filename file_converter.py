697626f765ee9bd2cd38ab669cf52ab8eaba9049
zerogitzero <102856606+zerogitzero@users.noreply.github.com>
2022-04-02 12:05:59 +0100

    Added location where to save audio files

--- file_converter.py
+++ file_converter.py
@@ -1,6 +1,8 @@
 from moviepy.editor import VideoFileClip
 
+location = "D:/Audiost/"
+
 def convert_to_mp3(filename):
-    clip = VideoFileClip(filename)
-    clip.audio.write_audiofile(filename[:-4] + ".mp3")
+    clip = VideoFileClip(location + filename)
+    clip.audio.write_audiofile(location + filename[:-4] + ".mp3")
     clip.close()
